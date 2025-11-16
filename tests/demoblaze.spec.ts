1752bf15d13380a5e431293fb63cccbd388c2c54
stefanos <stefanastasiadis@gmail.com>
2025-11-16 23:18:43 +0100

    new demoblaze login class file and test file

---
+++ tests/demoblaze.spec.ts
@@ -0,0 +1,22 @@
+import {test,expect} from '@playwright/test';
+
+
+import {login_demoblaze} from './demoblaze_login_class.js';
+
+
+test('demoblaze site tests',async({page})=>{
+
+    //navigate to demoblaze site
+    await page.goto('https://demoblaze.com/index.html');
+   
+const userlogin1=new login_demoblaze(page);
+
+userlogin1.username='samfischer';
+userlogin1.password='splintercell';
+
+const loggedinuser=await userlogin1.login(userlogin1.username,userlogin1.password);
+
+await expect(loggedinuser).toEqual(`Welcome ${userlogin1.username}`);
+
+
+})
