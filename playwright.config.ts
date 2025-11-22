7ca1c96cfeeca42fcda12e9a2f51c1fdd35ed21a
stefanos <stefanastasiadis@gmail.com>
2025-11-22 23:41:04 +0100

    nothing

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: ['html'],
+  reporter: [['html']],
 
   // IMPORTANT: Ensure the output directory for test results is cleared
   // This prevents merging results from previous runs.
